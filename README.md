ab3a147a0fde60de8a38e741de0c5c5dfa7393b0
cmyui <josh@cmyui.com>
2018-01-11 09:22:10 -0500

    read                                 moi

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # kablaze
 
-Um.... Yea basically.. Let's not talk about it
+Hey! Honestly I have no idea what im doing here but it's just something im doing to learn java. It's pretty cringy and thats basically the goal of it.
+Enjoy getting as far as you can before the game ends on you :^)
--- kablaze.java
+++ kablaze.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 
 /*
 * Author: cmyui#5585 // https://akatsuki.pw
-* Hey! Honestly I have no idea what im doing here but it's just something im doing to learn java. It's pretty cringy and thats basically the goal of it.
-* Enjoy getting as far as you can before the game ends on you :^)
 */
 
 public class kablaze {
